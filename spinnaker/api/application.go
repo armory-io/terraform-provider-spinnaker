1a9d913863b9e388d0bc7f5ea17341a56b8702d7
Johann Weging <johann@weging.com>
2019-03-14 15:38:34 +0100

    hack(API): Retry reading the application after creation on 403

--- spinnaker/api/application.go
+++ spinnaker/api/application.go
@@ -68,6 +68,26 @@ func CreateApplication(client *gate.GatewayClient, applicationName string, appli
 		return fmt.Errorf("Encountered an error saving application, task output was: %v\n", task)
 	}
 
+	// HACK:
+	// When creating an application with group permissions terraform fails to
+	// read the application due to a HTTP 403. It looks like the permissions
+	// are set without refreshing the cache.
+	//
+	// It looks like the responses of the gate API are cached.
+	// Try accessing the application until the cache timeout is reached
+	// and the API allows access to the application.
+	// The default redis cache timeout is 30 seconds.
+	attempts = 0
+	for attempts < 10 {
+		_, resp, _ := client.ApplicationControllerApi.GetApplicationUsingGET(client.Context, applicationName, map[string]interface{}{})
+		if resp != nil && resp.StatusCode != 403 {
+			break
+		}
+
+		attempts += 1
+		time.Sleep(time.Duration(10) * time.Second)
+	}
+
 	return nil
 }
 
