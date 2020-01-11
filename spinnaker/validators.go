860aa25238807a1b9a7309e27b1f93209bd040df
Paul Rondeau <paulrondeau07@gmail.com>
2020-01-10 16:47:15 -0800

    added dash to allowable application name regex

--- spinnaker/validators.go
+++ spinnaker/validators.go
@@ -7,8 +7,8 @@ import (
 
 func validateApplicationName(v interface{}, k string) (ws []string, errors []error) {
 	value := v.(string)
-	if !regexp.MustCompile(`^[a-zA-Z0-9]+$`).MatchString(value) {
-		errors = append(errors, fmt.Errorf("Only alphanumeric characters allowed in %q", k))
+	if !regexp.MustCompile(`^[a-zA-Z0-9-]+$`).MatchString(value) {
+		errors = append(errors, fmt.Errorf("Only alphanumeric characters or '-' allowed in %q", k))
 	}
 	return
 }
